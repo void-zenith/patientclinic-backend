059eaa149392c65932c88abf8bfafdda0fdd1a40
void-zenith <zenithrajbhandari@gmail.com>
2023-11-09 15:01:05 -0500

    added get single  record

--- index.js
+++ index.js
@@ -220,3 +220,27 @@ server.get("/record", (req, res, next) => {
     throw error;
   }
 });
+
+//get single record
+server.get("/record/:id", (req, res, next) => {
+  try {
+    // Find a single user by their id in db
+    RecordModel.findOne({ _id: req.params.id })
+      .then((data) => {
+        if (data) {
+          // Send the user if no issues
+          res.send(data);
+        } else {
+          // Send 404 header if the user doesn't exist
+          res.send(404);
+        }
+        return next();
+      })
+      .catch((error) => {
+        console.log("error: " + error);
+        return next(new Error(JSON.stringify(error.errors)));
+      });
+  } catch (error) {
+    throw error;
+  }
+});
